ca6b17b0f5583038646fe1d6f4b60b265ee7b37d
Jon Nichols <jnichols@me.com>
2013-04-02 01:32:45 -0400

    General improvements to the Facebook authenticator.

--- optional/facebook/src/FacebookAuthenticator.kt
+++ optional/facebook/src/FacebookAuthenticator.kt
@@ -16,19 +16,31 @@ import node.Configuration
  * Middleware for authenticating requests that include a Facebook session key. Validates the key
  * using a shared secret, then assigns the user id to facebookUser in the request.
  */
-class FacebookAuthenticator(val appId: String, val secret: String,
+class FacebookAuthenticator(val appId: String, val secret: String, val redirectUrl: String,
                             val success: (res: Response, accessToken: String)->Unit,
                             val fail: (res: Response)->Unit): Handler {
-  val json = ObjectMapper()
+  val json = ObjectMapper();
+
+  {
+    this.log("Facebook authenticator. appId: $appId, redirectUrl: $redirectUrl")
+  }
 
   override fun exec(req: Request, res: Response, next: () -> Unit) {
+    this.log("Facebook authenticator. appId: $appId, redirectUrl: $redirectUrl")
+
     val code = req.param("code") as String?
     if (code != null) {
       val response = HttpClient.get("https://graph.facebook.com/oauth/access_token").
         query("client_id", appId).
         query("client_secret", secret).
-        query("redirect_uri", "http://localhost:4000/users/login/facebook").
-        query("code", code).form()
+        query("redirect_uri", redirectUrl).
+        query("code", code).withErrorHandler({ r ->
+        if (r.contentType() == "text/javascript") {
+          throw IOException(r.text())
+        } else {
+          HttpClient.defaultErrorHandler(r)
+        }
+      }).form()
 
       val accessToken = response.get("access_token")
       if (accessToken != null) {
@@ -39,30 +51,6 @@ class FacebookAuthenticator(val appId: String, val secret: String,
     } else {
       fail(res)
     }
-//
-//    var signature = req.param(paramName) as? String
-//    if (signature == null) {
-//      signature = req.cookie(paramName)
-//    }
-//    signature = signature?.replace("-", "+")?.replace("_", "/")?.trim()
-//    if (signature != null) {
-//      val bits = signature!!.split("\\.").toMap("token", "data");
-//
-//      val expected = bits["data"]?.hmac("HmacSHA256", secret, "base64")?.replace("=", "")
-//      if (expected == bits["token"]) {
-//        try {
-//          val jsonTree = json.readTree(bits["data"]?.decode("base64"))
-//          val uid = jsonTree?.get("user_id")?.asText()
-//          if (uid != null) {
-//            req.attributes["fbSessionKey"] = uid!!
-//            req.attributes["fbUid"] = uid!!
-//          }
-//        } catch (e: IOException) {
-//          this.log(message = "Error parsing json key", t = e)
-//        }
-//      }
-//    }
-//    next()
   }
 
   class object {
@@ -73,7 +61,7 @@ class FacebookAuthenticator(val appId: String, val secret: String,
     fun withConfig(success: (res: Response, accessToken: String)->Unit,
                    fail: (res: Response)->Unit): FacebookAuthenticator {
       return FacebookAuthenticator(Configuration.string("facebook.app.id"),
-          Configuration.string("facebook.app.secret"), success, fail)
+          Configuration.string("facebook.app.secret"), Configuration.string("facebook.loginurl"), success, fail)
     }
   }
 }
